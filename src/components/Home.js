12285e7ce976c6b5df5c1efd4abd8b5249ed8987
Mostafizur rahman <sourav10681@gmail.com>
2022-05-19 04:06:05 +0600

    added item from localhost

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import ManageToDo from './ManageToDo';
 
 const Home = () => {
     const { register, handleSubmit } = useForm();
 
     const onSubmit = data => {
         console.log(data)
-        // const url = `https://quiet-beyond-84134.herokuapp.com/inventory`;
-        // fetch(url, {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(data)
-        // })
-        //     .then(res => res.json())
-        //     .then(result => {
-        //         console.log(result)
-        //     })
-        // toast('Your New Inventory Item is Added')
+        const url = 'https://localhost:5000/todo';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
     };
     return (
         <div className='container mt-5 w-50 mx-auto'>
-            <h2 className='text-center text-success text-uppercase'>Please Add a Inventory</h2>
+            <h2 className='text-center text-success text-uppercase'>Add ToDo</h2>
             <form className='d-flex flex-column mt-3 mb-5' onSubmit={handleSubmit(onSubmit)}>
                 <input className='mb-2 border p-2 rounded' type='text' placeholder='Product Name' {...register("name", { required: true, maxLength: 20 })} />
                 <textarea className='mb-2 border' placeholder='Description' {...register("description",)} />
-                <input type="submit" value='Add Inventory' className='btn-success py-2 text-center rounded' />
+                <input type="submit" value='Add Todos' className='btn-primary py-2 text-center rounded' />
             </form>
+            <ManageToDo></ManageToDo>
         </div>
     );
 };
---
+++ src/components/ManageToDo.js
@@ -0,0 +1,74 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ManageToDo = () => {
+    const [courses, setCourses] = useState([])
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch('http://localhost:5000/todo')
+            .then(res => res.json())
+            .then(data => {
+                setCourses(data);
+            })
+    }, [])
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure yo want to delete?')
+        if (proceed) {
+            // console.log('delete item', id)
+            const url = `http://localhost:5000/todo/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deleteCount > 0) {
+                        const remaining = courses.filter(course => course._id !== id);
+                        setCourses(remaining)
+                    }
+                })
+        }
+    }
+    return (
+        <div className='container my-5'>
+
+            <h2 className='text-center text-secondary mb-3 text-uppercase text-success'>Inventory Statement</h2>
+            <hr style={{ width: '35%', margin: '0  auto' }} className='mb-4' />
+
+            <table className=''>
+
+                <thead>
+
+                    <tr className='bg-custom text-light'>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th>Action</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        courses.map(course =>
+                            <tr>
+                                <td data-label="Name">{course.name}</td>
+                                <td data-label="Name">{course.description}</td>
+
+                                <td>
+                                    <button onClick={() => handleDelete(course._id)} className=' text-dark btn fs-6 border-3 border-danger rounded-pill'><FontAwesomeIcon className=' fs-6 text-danger ' style={{ cursor: "pointer" }} icon={faTrash}></FontAwesomeIcon><span>Delete</span></button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+
+            </table>
+        </div>
+    );
+};
+
+export default ManageToDo;
